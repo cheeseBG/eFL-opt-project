af17b6aaafc6b333e012cbdd32aa703e32389605
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2022-11-30 15:28:52 +0900

    update

--- efl.py
+++ efl.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     # Set similarity threshold
     thres = 0
     if args.model == 'mlp':
-        thres = 0.79
+        thres = 0.94
     else:
         thres = 0.9999
 
