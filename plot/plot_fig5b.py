0a5fe96bafc630723c823b6ca4bc77c9cec4833d
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2022-12-01 12:42:49 +0900

    update data

--- plot/plot_fig5b.py
+++ plot/plot_fig5b.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-def loss_iter_plot(tfl_loss_list, dtfl_loss_list):
+def loss_iter_plot(tfl_loss_list, dtfl_loss_list, efl_loss_list):
     '''
     :return: present loss-iter plot
     '''
@@ -11,8 +11,10 @@ def loss_iter_plot(tfl_loss_list, dtfl_loss_list):
     plt.figure()
     plt.plot(range(len(dtfl_loss_list)), dtfl_loss_list, label='Traditional FL')
     plt.plot(range(len(tfl_loss_list)), tfl_loss_list, label='FL without dirty labels')
-    plt.ylabel('Training Loss')
-    plt.xlabel('Iterations')
+    plt.plot(range(len(efl_loss_list)), efl_loss_list, label='eFL')
+
+    plt.ylabel('Training Loss', fontsize=20)
+    plt.xlabel('Iterations', fontsize=20)
     plt.grid()
     plt.legend()
     plt.show()
@@ -21,6 +23,10 @@ def loss_iter_plot(tfl_loss_list, dtfl_loss_list):
 if __name__=='__main__':
     tfl_df = pd.read_csv('../results/tfl_nodirt_mlp.csv')
     dtfl_df = pd.read_csv('../results/tfl_dirty70_mlp.csv')
+    efl_df = pd.read_csv('../results/efl_dirty70_mlp.csv')
+
     tfl_loss_list = tfl_df['train_loss'].to_list()
     dtfl_loss_list = dtfl_df['train_loss'].to_list()
-    loss_iter_plot(tfl_loss_list, dtfl_loss_list)
+    efl_loss_list = efl_df['train_loss'].to_list()
+
+    loss_iter_plot(tfl_loss_list, dtfl_loss_list, efl_loss_list)
